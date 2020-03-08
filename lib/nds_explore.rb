9a9e91a1e915c4afc171211c1565375929497ce8
John Tom Jr <johntjr.22@gmail.com>
2020-03-08 01:19:47 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,7 +10,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  movies = print_first_directors_movie_titles[0][:movies]
+  movies = directors_database[0][:movies]
   index = 0 
   while index < movies.length do
     titles = [:movies][:index]
