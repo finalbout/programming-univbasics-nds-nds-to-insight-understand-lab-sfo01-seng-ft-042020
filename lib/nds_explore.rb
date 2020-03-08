8e605c5c036df2ce97c85b4be58fffbc707ea7c8
John Tom Jr <johntjr.22@gmail.com>
2020-03-08 01:17:52 +0000

    Automatically backed up by Learn

--- lib/nds_explore.rb
+++ lib/nds_explore.rb
@@ -10,6 +10,7 @@ def pretty_print_nds(nds)
 end
 
 def print_first_directors_movie_titles
-  movies = print_first_directors_movie_titles[0]
+  movies = print_first_directors_movie_titles[0][:movies]
+  while 
   
 end
